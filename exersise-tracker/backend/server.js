3256fe8748f248e31fb2ae34187ec4c8106312c6
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-02-01 23:03:56 +0100

    Initial commit of the exercise application

---
+++ exersise-tracker/backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// setup database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser : true, useCreateIndex : true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connection established.');
+} );
+
+app.listen( port, () => {
+    console.log(`Server is running on port : ${port}`);
+});
