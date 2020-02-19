67d6c4c5d19068755b2945d0fa8d584a86c62921
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-02-19 23:31:29 +0100

    Adding excercise router file

--- exersise-tracker/backend/routes/excercises.js
+++ exersise-tracker/backend/routes/excercises.js
@@ -9,6 +9,39 @@ router.route('/').get( (req, res) => {
     .catch( err => res.status(400).json('Error' + err) );
 });
 
+router.route('/:id').get( (req, res) => {
+
+    Excercise.findById(req.params.id)
+    .then( excercise => res.json(excercise) )
+    .catch( err => res.status(400).json('Error' + err) );
+});
+
+router.route('/:id').delete( (req, res) => {
+
+    Excercise.findByIdAndDelete(req.params.id)
+    .then( excercises => res.json('Excercise was deleted.') )
+    .catch( err => res.status(400).json('Error' + err) );
+});
+
+router.route('/update/:id').post((req, res) => {
+
+    Excercise.findById(req.params.id)
+    .then( excercise => {
+
+        excercise.username = req.body.username;
+        excercise.description = req.body.description;
+        excercise.duration = Number(req.body.duration);
+        excercise.date = Date.parse(req.body.date);
+
+        excercise.save()
+        .then( () => res.json('The excercise updated !') )
+        .catch( err => res.status(400).json('Error' + err) );
+
+    })
+    .catch( err => res.status(400).json('Error' + err) );
+});
+
+
 router.route('/add').post( (req, res) => {
 
     const username = req.body.username;
