4b6ec939b2c9aca9fb043a6a4e3121e823849b40
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-03-23 22:44:29 +0100

    Added front end react components for creating a user and an exercise.

---
+++ exersise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class EditExercise extends Component {    
+    render(){
+        return (
+            <div>
+                <p>On Edit Exercise component </p>
+            </div>
+        )
+    }
+}
